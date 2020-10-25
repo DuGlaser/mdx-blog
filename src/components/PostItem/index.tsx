a4538e7c76cdf45258d2ac43f0af854ec2707c11
DuGlaser <duglaser0618@gmail.com>
2020-10-26 00:02:49 +0900

    :recycle: Fix the PostItem style

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const PostItem: React.FC<Props> = ({ post }) => {
   return (
-    <li className="w-full px-2 py-4 mx-auto mt-3 bg-white border border-gray-400 rounded-lg md:p-4 sm:py-3">
+    <li className="w-full px-2 py-4 mx-auto mt-3 bg-white border border-gray-400 rounded-lg cursor-pointer md:p-4 sm:py-3">
       <Link
         as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
         href={`/posts/[slug]`}
@@ -17,15 +17,14 @@ const PostItem: React.FC<Props> = ({ post }) => {
             <a>{post.data.title}</a>
           </h2>
           <div className="mb-2">
-            {post.data.tags.map((tag: string) => (
-              <div
-                key={tag}
-                className="p-1 text-sm text-gray-600 hover:text-black"
-              >
-                <span className="text-opacity-50">#</span>
-                {tag}
-              </div>
-            ))}
+            <div className="flex flex-row p-1 ">
+              {post.data.tags.map((tag: string) => (
+                <div key={tag} className="mr-2 text-sm text-gray-600">
+                  <span className="text-opacity-50">#</span>
+                  {tag}
+                </div>
+              ))}
+            </div>
           </div>
           <a>{post.data.description}</a>
         </div>
