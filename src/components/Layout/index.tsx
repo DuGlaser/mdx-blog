409656c2f4ed6f612c422a3fd5d322fd31278dd5
DuGlaser <duglaser0618@gmail.com>
2020-11-01 11:36:31 +0900

    :sparkles: Add the link to the header title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,11 @@ const Layout = ({ children }: any) => {
     <div className="flex flex-col min-h-screen bg-gray-100">
       <header className="w-full text-gray-100 bg-gray-900 shadow body-font">
         <div className="container flex flex-col flex-wrap items-center justify-between p-5 mx-auto md:flex-row">
-          <h1 className="text-xl font-black text-white">永遠にWIP</h1>
+          <h1 className="text-xl font-black text-white">
+            <Link href="/">
+              <a>永遠にWIP</a>
+            </Link>
+          </h1>
           <nav className="flex mt-3 md:mt-0">
             <Link href="/about">
               <a className="mr-10">ABOUT</a>
