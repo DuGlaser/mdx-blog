98f00b9b297c9b199bc9e758b45eb00e12c257d2
DuGlaser <duglaser0618@gmail.com>
2020-11-28 02:17:52 +0900

    :sparkles: Add the Iframely component

---
+++ src/components/Iframely/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+
+const Iframely: React.FC = () => {
+  useEffect(() => {
+    if (window && window.iframely) {
+      window.iframely.load();
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <script
+          src="//cdn.iframe.ly/embed.js?iframe=1&omit_script=1"
+          async
+        ></script>
+      </Head>
+    </React.Fragment>
+  );
+};
+
+export default Iframely;
